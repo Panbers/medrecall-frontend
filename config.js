d064eaac7815f434d2e16e992b44041bc9f4bce1
Panbers <56555723+Panbers@users.noreply.github.com>
2025-10-27 16:42:03 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.ENV = {
-  API_URL: "http://localhost:3000",
-  MP_PUBLIC_KEY: "TEST-d7b8f465-ce60-4c15-b115-b37afb2e7101",
-  DEBUG: true
-};
+// config.js
+export const API_BASE =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://medrecall-backend.onrender.com";
